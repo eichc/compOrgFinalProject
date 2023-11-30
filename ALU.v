deabc127a960d16e54ee482095b518b9927c4341
eichc <eich.cameron@gmail.com>
2023-11-30 18:33:29 -0500

    alu

--- ALU.v
+++ ALU.v
@@ -1,11 +1,18 @@
-module ALU (ac, mbr, Op, clk);
-parameter op_size = 4;
+module ALU (ac, mbr, clk, control);
 input clk;
 input [15:0] ac;
-input [op_size-1:0] Op;
-inout [15:0] mbr;
+input [2:0] control
+inout reg [15:0] mbr;
 
-
+always @(posedge clk) begin
+    case (control)
+        3'b000: ac <= ac + mbr;
+        3'b001: ac <= ac - mbr;
+        3'b010: ac <= ac & mbr;
+        3'b011: ac <= ac | mbr;
+        3'b100: ac <= ~ac;
+    endcase
+end
 
 
 
