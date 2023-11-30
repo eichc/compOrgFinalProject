7f5a4b83e301a40024e635a1ba5a9fa292f658bf
eichc <eich.cameron@gmail.com>
2023-11-30 17:46:10 -0500

    Create ALU.v

---
+++ ALU.v
@@ -0,0 +1,13 @@
+module ALU (Out, In1, In2, COut, CIn, Op);
+parameter op_size = 4;
+input [31:0] In1, In2;
+input [op_size-1:0] Op;
+input CIn;
+output [31:0] Out;
+output Cout;
+
+
+
+
+
+endmodule
