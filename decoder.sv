632af2a6cf10726eb4813bc2d92b3d2d8e392616
eichc <eich.cameron@gmail.com>
2023-12-06 16:44:08 -0500

    decoder

--- decoder.sv
+++ decoder.sv
@@ -1,8 +1,7 @@
-// Adopted from https://circuitcove.com/design-examples-decoders/
 `timescale 1 ns / 1 ps
 
 module decoder
-  # (parameter ENCODE_WIDTH = 4,
+  # (parameter ENCODE_WIDTH = 2,
      parameter DECODE_WIDTH = 2**ENCODE_WIDTH
     )
 
@@ -14,5 +13,15 @@ module decoder
   localparam latency = 1;
 
   assign #latency out = 'b1<<in;
+
+    always @(in) begin
+        case (in)
+            2'b00: out = 4'b1000
+            2'b01: out = 4'b0100
+            2'b10: out = 4'b0010
+            2'b01: out = 4'b0001
+            default: out = 4'b0000
+        endcase
+    end
 
 endmodule
