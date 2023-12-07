8e5cc55b838a65030c140f7de633824854853293
eichc <eich.cameron@gmail.com>
2023-12-07 15:09:40 -0500

    decoder

--- decoder.sv
+++ decoder.sv
@@ -6,21 +6,17 @@ module decoder
     )
 
   (
-    input  [ENCODE_WIDTH-1:0] in,
-    output [DECODE_WIDTH-1:0] out
+    input [ENCODE_WIDTH-1:0] in,
+    output reg [DECODE_WIDTH-1:0] out
   );
 
-  localparam latency = 1;
-
-  assign #latency out = 'b1<<in;
-
-    always @(in) begin
+  always @(in) begin
         case (in)
-            2'b00: out = 4'b1000
-            2'b01: out = 4'b0100
-            2'b10: out = 4'b0010
-            2'b01: out = 4'b0001
-            default: out = 4'b0000
+            2'b00: out <= 4'b0001;
+            2'b01: out <= 4'b0010;
+            2'b10: out <= 4'b0100;
+            2'b11: out <= 4'b1000;
+            default: out <= 4'b0000;
         endcase
     end
 
